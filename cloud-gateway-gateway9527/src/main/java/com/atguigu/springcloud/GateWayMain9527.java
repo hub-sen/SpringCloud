aa2d9749f9caea6664b1222118812a1ee411942b
shishi <1107474494@qq.com>
2020-04-17 20:52:54 +0800

    GateWay 服务网关配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <pre>
+ * Description
+ * @author shishi
+ * 2020/4/17 20:49
+ * </pre>
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
